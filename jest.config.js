201197cdeb72c6cb995821423c5fec7c31a7a1da
Erin Millard <contact@erin.id.au>
2023-08-29 18:12:55 +1000

    Upgrade Jest config to fix hangs

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,1 @@
-import config from "@snout/jest-config";
-
-export default {
-  ...config,
-  transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        useESM: true,
-        tsconfig: "test/tsconfig.json",
-      },
-    ],
-  },
-};
+export { default } from "@snout/jest-config";
